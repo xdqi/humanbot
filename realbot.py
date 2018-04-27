ee060299edbe9bda8c10966231cb6364d0d0d804
Alexander Qi <xdqi@users.noreply.github.com>
2018-04-27 18:48:13 +0800

    Handle edited messages in real bot

--- realbot.py
+++ realbot.py
@@ -14,6 +14,7 @@ from utils import upload_pic, ocr, report_exception, AdminCommandHandler, show_c
 import cache
 from models import update_user_real, update_group_real
 import admin
+import models
 import humanbot
 
 logger = getLogger(__name__)
@@ -34,20 +35,18 @@ def update_group(bot: Bot, chat_id: int):
 
 
 def message(bot: Bot, update: Update):
-    msg = update.message  # type: Message
-    user = msg.from_user  # type: User
-    humanbot.insert_message(msg.chat_id, msg.message_id, user.id, msg.text, msg.date)
-    update_user(user)
-    update_group(bot, msg.chat_id)
+    if hasattr(update, 'message'):
+        msg = update.message  # type: Message
+        flag = models.ChatFlag.new
+    else:
+        msg = update.edited_message  # type: Message
+        flag = models.ChatFlag.edited
 
-
-def picture(bot: Bot, update: Update):
-    msg = update.message  # type: Message
     user = msg.from_user  # type: User
-    caption = msg.caption or ''  # in case of `None`
-    text = caption
+    text = msg.text
 
     if msg.photo:  # type: List[PhotoSize]
+        text = msg.caption or ''  # in case of `None`
         photo = max(msg.photo, key=lambda p: p.file_size)  # type: PhotoSize
         file = bot.get_file(photo.file_id)
 
@@ -62,9 +61,9 @@ def picture(bot: Bot, update: Update):
         fullpath = upload_pic(buffer, path, filename)
         result = ocr(fullpath)
         logger.info('ocr result:\n%s', result)
-        text = result + '\n' + caption
+        text = result + '\n' + text
 
-    humanbot.insert_message(msg.chat_id, msg.message_id, user.id, text, msg.date)
+    humanbot.insert_message(msg.chat_id, msg.message_id, user.id, text, msg.date, flag)
     update_user(user)
     update_group(bot, msg.chat_id)
 
@@ -106,10 +105,10 @@ def main():
     dispatcher.add_handler(AdminCommandHandler('workers', humanbot.workers_handler))
     dispatcher.add_handler(AdminCommandHandler('help', show_commands_handler))
 
-    message_handler = MessageHandler(filters=Filters.text, callback=message)
+    message_handler = MessageHandler(filters=Filters.text, allow_edited=True, callback=message)
     dispatcher.add_handler(message_handler)
 
-    picture_handler = MessageHandler(filters=Filters.photo | Filters.document, callback=picture)
+    picture_handler = MessageHandler(filters=Filters.photo | Filters.document, allow_edited=True, callback=message)
     dispatcher.add_handler(picture_handler)
 
     all_handler = MessageHandler(filters=Filters.all, callback=log_message)
